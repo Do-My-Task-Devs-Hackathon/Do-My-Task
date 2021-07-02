d4936aa2d10e2254f2e65fd314281ecfa4760c5e
James <41566813+jameszu@users.noreply.github.com>
2021-07-03 11:11:07 +1200

    Authentication in frontend #8
    Finished authentication in the frontend with Auth0

---
+++ frontend/src/components/LogoutButton.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from '@material-ui/core';
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+  const { user } = useAuth0();
+  const { name } = user;
+  return (
+    <div style={{display: 'flex'}}>
+        <p style={{fontFamily: 'Arial', marginRight: '10px'}}>
+                Hello, {name}
+        </p>
+        {/* {JSON.stringify(user.sub, null, 2)} */}
+
+        <Button 
+            variant="contained" 
+            size="small"
+            style={{marginTop: '5px', maxWidth: '100px', maxHeight: '40px', minWidth: '50px', minHeight: '20px'}}
+            onClick={() => logout({ returnTo: window.location.origin })}
+            >
+        Log Out
+        </Button>
+        
+    </div>
+  );
+};
+
+export default LogoutButton;
