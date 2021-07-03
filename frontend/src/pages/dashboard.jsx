df4f9adbec26de1f14b02dbdffd70bc148c634b7
JoshXLim <joshxi_00@yahoo.com>
2021-07-03 17:02:03 +1200

    Task card, dashboard mousepopover:
     - MousePopover for popover events
    
     - Changed color for task card and different format for initation and host
    
     - Added help icon and members list section for dashboard

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -4,7 +4,9 @@ import TaskCard from '../components/TaskCard';
 import Grid from '@material-ui/core/Grid';
 import { Button } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
+import HelpIcon from '@material-ui/icons/Help';
 import Footer from '../components/Footer';
+import MousePopover from '../components/MousePopover';
 
 const useStyles = makeStyles((theme) => ({
     heading: {
@@ -24,6 +26,9 @@ const useStyles = makeStyles((theme) => ({
         marginTop: "auto",
         marginBottom: "auto",
         padding: "10px",
+        "&:hover": {
+            backgroundColor: "#547e8a",
+        }
     },
 
     sectionPadding: {
@@ -33,8 +38,22 @@ const useStyles = makeStyles((theme) => ({
         paddingBottom: "80px",
     },
 
+    sectionTitle: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+    },
+
+    helpIcon: {
+        marginLeft: "10px",
+        marginRight: "10px",
+        "&:hover": {
+            cursor: "pointer",
+        },
+    },
+
     hostedSection: {
-        backgroundColor: "#c8d8e4",
+        backgroundColor: "#f2f2f2",
     },
 
     taskGrid: {
@@ -42,8 +61,9 @@ const useStyles = makeStyles((theme) => ({
     },
 
     invitedSection: {
-        backgroundColor: "#f2f2f2",
+        backgroundColor: "#c8d8e4",
     },
+    
 
     footer: {
         display: "flex",
@@ -70,62 +90,103 @@ const useStyles = makeStyles((theme) => ({
 export default function Dashboard() {
     const classes = useStyles();
 
+    const [anchorElHostHelp, setAnchorElHostHelp] = React.useState(null);
+    const handlePopoverOpenHost = (event) => {
+        setAnchorElHostHelp(event.currentTarget);
+    };
+    const handlePopoverCloseHost = () => {
+        setAnchorElHostHelp(null);
+    };
+
+    const [anchorElHostInvi, setAnchorElHostInvi] = React.useState(null);
+    const handlePopoverOpenInvi = (event) => {
+        setAnchorElHostInvi(event.currentTarget);
+    };
+    const handlePopoverCloseInvi = () => {
+        setAnchorElHostInvi(null);
+    };
+
     return (
         <div>
+            <section className={`${classes.invitedSection} ${classes.sectionPadding}`}>
+                <div className={classes.hostHeader}>
+                    <div className={classes.sectionTitle}>
+                        <h1 className={classes.heading}>Members of <u>Project A</u>:</h1>
+                        <HelpIcon className={classes.helpIcon} onMouseEnter={handlePopoverOpenHost} onMouseLeave={handlePopoverCloseHost} />
+                    </div>
+                </div>
+            </section>
+
             <section className={`${classes.hostedSection} ${classes.sectionPadding}`}>
                 <div className={classes.hostHeader}>
-                    <h1 className={classes.heading}>Hosted Tasks:</h1>
+                    <div className={classes.sectionTitle}>
+                        <h1 className={classes.heading}>Hosted Tasks:</h1>
+                        <HelpIcon className={classes.helpIcon} onMouseEnter={handlePopoverOpenHost} onMouseLeave={handlePopoverCloseHost} />
+                    </div>
+
+
                     <Button className={classes.createBtn}><AddIcon />Create Task</Button>
                 </div>
-                <Grid container spacing={5}>
-                    <Grid item lg={3} md={4} sm={6} xs={12} className={classes.taskGrid}>
+                <Grid container spacing={3}>
+                    <Grid item lg={3} md={3} sm={6} xs={12} className={classes.taskGrid}>
                         <TaskCard host="Weak af guy who can't finish this task alone" description="I don't know what I am doing but if anyone can help me right now, imma pay him $10000000000." />
                     </Grid>
 
-                    <Grid item lg={3} md={4} sm={6} xs={12} className={classes.taskGrid}>
+                    <Grid item lg={3} md={3} sm={6} xs={12} className={classes.taskGrid}>
                         <TaskCard host="Weak af guy who can't finish this task alone" description="I don't know what I am doing but if anyone can help me right now, imma pay him $10000000000." />
                     </Grid>
 
-                    <Grid item lg={3} md={4} sm={6} xs={12} className={classes.taskGrid}>
+                    <Grid item lg={3} md={3} sm={6} xs={12} className={classes.taskGrid}>
                         <TaskCard host="Weak af guy who can't finish this task alone" description="I don't know what I am doing but if anyone can help me right now, imma pay him $10000000000." />
                     </Grid>
 
-                    <Grid item lg={3} md={4} sm={6} xs={12} className={classes.taskGrid}>
+                    <Grid item lg={3} md={3} sm={6} xs={12} className={classes.taskGrid}>
                         <TaskCard host="Weak af guy who can't finish this task alone" description="I don't know what I am doing but if anyone can help me right now, imma pay him $10000000000." />
                     </Grid>
 
-                    <Grid item lg={3} md={4} sm={6} xs={12} className={classes.taskGrid}>
+                    <Grid item lg={3} md={3} sm={6} xs={12} className={classes.taskGrid}>
                         <TaskCard host="Weak af guy who can't finish this task alone" description="I don't know what I am doing but if anyone can help me right now, imma pay him $10000000000." />
                     </Grid>
                 </Grid>
             </section>
 
             <section className={`${classes.invitedSection} ${classes.sectionPadding}`}>
-                <h1 className={classes.heading}>Invited Tasks</h1>
+                <div className={classes.sectionTitle}>
+                    <h1 className={classes.heading}>Invited Tasks:</h1>
+                    <HelpIcon className={classes.helpIcon} onMouseEnter={handlePopoverOpenInvi} onMouseLeave={handlePopoverCloseInvi}/>
+                </div>
                 <Grid container spacing={5}>
-                    <Grid item lg={3} md={4} sm={6} xs={12} className={classes.taskGrid}>
-                        <TaskCard host="Weak af guy who can't finish this task alone" description="I don't know what I am doing but if anyone can help me right now, imma pay him $10000000000." />
+                    <Grid item lg={3} md={3} sm={6} xs={12} className={classes.taskGrid}>
+                        <TaskCard isInvitation={true} host="Weak af guy who can't finish this task alone" description="I don't know what I am doing but if anyone can help me right now, imma pay him $10000000000." />
                     </Grid>
 
-                    <Grid item lg={3} md={4} sm={6} xs={12} className={classes.taskGrid}>
-                        <TaskCard host="Weak af guy who can't finish this task alone" description="I don't know what I am doing but if anyone can help me right now, imma pay him $10000000000." />
+                    <Grid item lg={3} md={3} sm={6} xs={12} className={classes.taskGrid}>
+                        <TaskCard isInvitation={true} host="Weak af guy who can't finish this task alone" description="I don't know what I am doing but if anyone can help me right now, imma pay him $10000000000." />
                     </Grid>
 
-                    <Grid item lg={3} md={4} sm={6} xs={12} className={classes.taskGrid}>
-                        <TaskCard host="Weak af guy who can't finish this task alone" description="I don't know what I am doing but if anyone can help me right now, imma pay him $10000000000." />
+                    <Grid item lg={3} md={3} sm={6} xs={12} className={classes.taskGrid}>
+                        <TaskCard isInvitation={true} host="Weak af guy who can't finish this task alone" description="I don't know what I am doing but if anyone can help me right now, imma pay him $10000000000." />
                     </Grid>
 
-                    <Grid item lg={3} md={4} sm={6} xs={12} className={classes.taskGrid}>
-                        <TaskCard host="Weak af guy who can't finish this task alone" description="I don't know what I am doing but if anyone can help me right now, imma pay him $10000000000." />
+                    <Grid item lg={3} md={3} sm={6} xs={12} className={classes.taskGrid}>
+                        <TaskCard isInvitation={true} host="Weak af guy who can't finish this task alone" description="I don't know what I am doing but if anyone can help me right now, imma pay him $10000000000." />
                     </Grid>
 
-                    <Grid item lg={3} md={4} sm={6} xs={12} className={classes.taskGrid}>
-                        <TaskCard host="Weak af guy who can't finish this task alone" description="I don't know what I am doing but if anyone can help me right now, imma pay him $10000000000." />
+                    <Grid item lg={3} md={3} sm={6} xs={12} className={classes.taskGrid}>
+                        <TaskCard isInvitation={true} host="Weak af guy who can't finish this task alone" description="I don't know what I am doing but if anyone can help me right now, imma pay him $10000000000." />
                     </Grid>
                 </Grid>
             </section>
 
-            <Footer/>
+            <Footer />
+
+            <MousePopover anchorEl={anchorElHostHelp} handlePopoverClose={handlePopoverCloseHost}>
+                <img src="https://user-images.githubusercontent.com/41566813/124340855-4cbb6580-dc0c-11eb-8979-2cbb5882bfb0.png" />
+            </MousePopover>
+
+            <MousePopover anchorEl={anchorElHostInvi} handlePopoverClose={handlePopoverCloseInvi}>
+                <img src="https://user-images.githubusercontent.com/41566813/124340855-4cbb6580-dc0c-11eb-8979-2cbb5882bfb0.png" />
+            </MousePopover>
         </div>
     )
 }
